f5889262a97d5b8a9f3594f09ac941497be6db88
nsuedu <642178633@qq.com>
2017-07-24 11:47:34 +0800

    delete node_modules

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM, { render } from 'react-dom'
 import { BrowserRouter as Router, Route, Link, Switch, NavLink } from 'react-router-dom'
-import createBrowserHistory from 'history/es/createBrowserHistory'
 
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
+import createBrowserHistory from 'history/es/createBrowserHistory'
 const history = createBrowserHistory();
 
-import { LayoutCustomize } from './src/__share'
+
 import RootRoute from './rootRoute'
 
 
@@ -22,7 +19,6 @@ render(
         {RootRoute.map((route, index) => (
           // 在一个给定的路径下渲染多个 <Route> ，而且每个
           // <Route> 的 component 属性都不相同。
-
           <Route
             key={index}
             path={route.path}
